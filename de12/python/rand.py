490d5d0cb374c04edb445d12721e3ed29a266132
miyayui <r202302009ck@jindai.jp>
2023-10-25 15:03:37 +0900

    20231025

--- de12/python/rand.py
+++ de12/python/rand.py
@@ -1,3 +1,18 @@
 import random
-a=("あああ","いいい","ううう")
-print(random.choice(a))
+
+# ゲームで使用する単語リスト
+words = ["python", "programming", "code", "computer", "keyboard", "practice", "challenge"]
+
+score=0
+while True:
+        target_word = random.choice(words)
+        print(f"次の文字を打て！: {target_word}")
+        
+        user_input = input()
+        if user_input == target_word:
+            print("○")
+            score+=1
+        else:
+            print("×")
+            score=-1
+
