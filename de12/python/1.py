f9dbf371712d7b8fd0a52a9c5008cb13c8fb3b17
miyayui <r202302009ck@jindai.jp>
2023-10-27 16:23:28 +0900

    20231027

--- de12/python/1.py
+++ de12/python/1.py
@@ -1,35 +1,44 @@
 import random
-
+import time
 # ゲームで使用する単語リスト
 words = ["python", "programming", "code", "computer", "keyboard", "practice", "challenge"]
 
 def main():
     print("Welcome to the Typing Game!")
     input("Press Enter to start...")
-
+    
+    
     while True:
         score = 0
+        game_duration=60
+
+        start_time=time.time()
+        end_time=start_time+game_duration
+
         incorrect = False
 
-        while True:
+        while time.time()<end_time:
             target_word = random.choice(words)
             print(f"Type this word: {target_word}")
 
             user_input = input()
             if user_input == target_word:
-                score += 1
-                print("Correct!")
+                    score += 1
+                    print("Correct!")
             else:
-                print("Incorrect!")
-                incorrect = True
-                break  # 間違えた場合、内部の無限ループを終了
-        print(f"Gane Over! Your score:{score}")
+                    print("Incorrect!")
+                    incorrect = True
+                    break  # 間違えた場合、内部の無限ループを終了
 
-        play_again = input("Do you want to play again? (yes/no): ").lower()
-        if play_again != "yes":
-            break
-
-    
+        if incorrect:
+            print(f"Gane Over! Your score:{score}")
+            play_again = input("Do you want to play again? (yes/no): ").lower()
+            if play_again != "yes":
+                break
+        else:
+             break
+        
+    print(f"Gane Over! Your score:{score}")
 
 if __name__ == "__main__":
     main()
